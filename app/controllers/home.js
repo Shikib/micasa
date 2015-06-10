26745dddccad5935a5e1a1c4372eee8fefe6543c
Qing Yi Li <qinggyili@gmail.com>
2015-06-10 13:35:51 -0700

    added materialize css

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -2,6 +2,9 @@ var express = require('express'),
   router = express.Router(),
   db = require('../models');
 
+// for UI widget maybe we dont even need keep it here for now.
+var qx = require('qooxdoo')
+
 module.exports = function (app) {
   app.use('/', router);
 };
@@ -9,9 +12,14 @@ module.exports = function (app) {
 router.get('/', function (req, res, next) {
 //  db.Article.findAll().then(function (articles) {
     res.render('index', {
-      title: 'Welcome to Micasa',
+      title: 'Micasa',
 //      articles: articles
     });
 //  });
 });
 
+function test(){
+	q("#button-page button").button().on("tap", logEvent("tap"));
+var menu = q("#menu").addClass("qx-menu").appendTo(document.body).hide();
+q("#menu-button").setMenu(menu);
+}
