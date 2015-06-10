d2b1256bb3cc18734395579579b6675c7d03a5c7
Shikib Mehri <mehrishikib@gmail.com>
2015-06-09 20:54:02 -0700

    wrote create staments for Accepts, Agency, Agent_Represents, Offer and Seller tables

--- app/models/create_table.js
+++ app/models/create_table.js
@@ -2,8 +2,38 @@ var mysqlModule = require("../../mysqlModule");
 
 exports.initialize_tables = function() {
   mysqlModule.getConnection(function(err, conn) {
-  conn.query("CREATE TABLE TEST (" +
-        "id int," +
-        "name VARCHAR(200))");
+    conn.query("CREATE TABLE Agency (" +
+                 "name varchar(20)," +
+                 "agencyRating tinyint," +
+                 "agencyID smallint PRIMARY KEY)");
+
+    conn.query("CREATE TABLE Agent_Represents (" +
+                 "agentID smallint," +
+                 "agentName varchar(20)," +
+                 "agentPhone char(10)," +
+                 "agentEmail varchar(15)," +
+                 "agencyID smallint PRIMARY KEY," +
+                 "agentRating integer," +
+                 "FOREIGN KEY (agencyID) REFERENCES Agency(agencyID))");                  
+
+    conn.query("CREATE TABLE Offer (" +
+                 "offerID smallint PRIMARY KEY," +
+                 "offerMessage varchar(200)," +
+                 "offerDate date not NULL)");
+
+    conn.query("CREATE TABLE Accepts (" +
+                 "offerID smallint PRIMARY KEY," +
+                 "sellerPhone char(10)," +
+                 "sellerName varchar(20)," +
+                 "FOREIGN KEY (sellerPhone, sellerName) references Seller(sellerPhone, sellerName)," +
+                 "FOREIGN KEY (offerID) references Offer(offerID))");
+
+    conn.query("CREATE TABLE Seller (" +
+                 "sellerEmail varchar(15)," +
+                 "sellerPhone char(10) not NULL," +
+                 "sellerName varchar(20) not NULL," +
+                 "PRIMARY KEY(sellerPhone, sellerName))");
+
+
   });
 };
