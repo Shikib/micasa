9f26aae9dac865ed7915c33a09b495942cf9837c
Shikib Mehri <mehrishikib@gmail.com>
2015-06-13 13:50:32 -0700

    implemented queries for retrieving properties in a city, city/province, city/province/country

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -2,17 +2,16 @@ var express = require('express'),
   router = express.Router(),
   db = require('../models');
 
+var mysqlModule = require('../../mysqlModule');
+
 module.exports = function (app) {
   app.use('/', router);
 };
 
 router.get('/', function (req, res, next) {
-//  db.Article.findAll().then(function (articles) {
     res.render('index', {
       title: 'Micasa',
-//      articles: articles
     });
-//  });
 });
 
 router.get('/search', function (req, res, next) {
@@ -23,13 +22,54 @@ router.get('/search', function (req, res, next) {
 
 router.get('/searching', function(req, res, next) {
   var val = req.query.search;
+  
   console.log(val);
-  console.log("!!!!!!!!!!!!!!!!!!!!!");
-  res.send("test");
+
+  console.log(val.split(',').length);
+
+    
+  // split up into multiple variables to query appropriately
+  var split_location = val.split(',');
+  
+  // only city specified
+  if (split_location.length == 1) {
+    mysqlModule.getConnection(function(err, conn) {
+      mysqlModule.query(conn, "SELECT * " +
+                              "FROM Property_HasA_Location " +
+                              "WHERE city = '" + split_location[0] + "';",
+                       res);
+
+      }); 
+   }
+ 
+  // city + province specified
+  else if (split_location.length == 2) {
+    mysqlModule.getConnection(function(err, conn) {
+      mysqlModule.query(conn, "SELECT * " +
+                              "FROM Property_HasA_Location " +
+                              "WHERE city = '" + split_location[0] + "' " +
+                              "AND province = '" + split_location[1] + "';",
+                       res);
+
+      }); 
+  }
+
+  // city + province specified
+  else {
+    mysqlModule.getConnection(function(err, conn) {
+      mysqlModule.query(conn, "SELECT * " +
+                              "FROM Property_HasA_Location " +
+                              "WHERE city = '" + split_location[0] + "' " +
+                              "AND province = '" + split_location[1] + "' " +
+                              "AND country = '" + split_location[2] + "';",
+                       res);
+
+      }); 
+   }
 });
 
 function test(){
 	q("#button-page button").button().on("tap", logEvent("tap"));
 var menu = q("#menu").addClass("qx-menu").appendTo(document.body).hide();
-q("#menu-button").setMenu(menu);
+ ("#menu-button").setMenu(menu);
 }
