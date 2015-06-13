cdc59086a322c847c36b7341c4280372fce9678c
qing yi li <qingyili@qing-yi-lis-iMac.local>
2015-06-12 22:40:45 -0700

    fixed populated tables still errors tho

--- app/models/populate_tables.js
+++ app/models/populate_tables.js
@@ -8,38 +8,23 @@ exports.populate_tables = function() {
     		   "INSERT INTO Agency VALUES ('AyyHouseNow', 2, 9456);" +
     		   "INSERT INTO Agency VALUES ('RealMax', NULL, 9567);");
 
-    conn.query("INSERT INTO Property_HasA_Location VALUES (8123, Y, 5, 7123, NULL, 2000, '6123', 
-    			'Fake Street', 'Canadia', 'Vancouveria', 'BC');" +
-    		   "INSERT INTO Property_HasA_Location VALUES (8234, Y, 0, 7234, 47, 500, '6234',
-    		   	'Highland Blvd', 'Canadia', 'Vancouveria', 'BC');" +
-    		   "INSERT INTO Property_HasA_Location VALUES (8345, N, 97, 7345, NULL, 10000, '6345',
-    		   	'Sunrise Lane', 'Amurnica', 'Portlandia', 'OR');" +  
-    		   "INSERT INTO Property_HasA_Location VALUES (8456, N, 10, 7456, 396, 1000, '6456',
-    		   	'Highland Blvd', 'Canadia', 'Vancouveria', 'BC');" +
-    		   "INSERT INTO Property_HasA_Location VALUES (8567, NULL, NULL, 7567, NULL, 5000, '6567',
-    		   	'Mansion Way', 'Amurnica', 'Seattlilia', 'WA');");
+    conn.query("INSERT INTO Property_HasA_Location VALUES (8123, Y, 5, 7123, NULL, 2000, '6123', 'Fake Street', 'Canadia', 'Vancouveria', 'BC');" +
+    		   "INSERT INTO Property_HasA_Location VALUES (8234, Y, 0, 7234, 47, 500, '6234', 'Highland Blvd', 'Canadia', 'Vancouveria', 'BC');" +
+    		   "INSERT INTO Property_HasA_Location VALUES (8345, N, 97, 7345, NULL, 10000, '6345', 'Sunrise Lane', 'Amurnica', 'Portlandia', 'OR');" +  
+    		   "INSERT INTO Property_HasA_Location VALUES (8456, N, 10, 7456, 396, 1000, '6456','Highland Blvd', 'Canadia', 'Vancouveria', 'BC');" +
+    		   "INSERT INTO Property_HasA_Location VALUES (8567, NULL, NULL, 7567, NULL, 5000, '6567',	'Mansion Way', 'Amurnica', 'Seattlilia', 'WA');");
 
-    conn.query("INSERT INTO Agent_Represents VALUES (1123, 'Alex Huang', 1113456789, 'ah@real.com',
-    	9123, 10);" +
-    		   "INSERT INTO Agent_Represents VALUES (1234, 'Katherine Lab', 1113334444, 'kl@wb.com',
-    	9234, 9);" +
-    		   "INSERT INTO Agent_Represents VALUES (1345, 'Joe Baker', 1112223333, 'jb@rNOW.com',
-    	9345, 5);" +
-    		   "INSERT INTO Agent_Represents VALUES (1456, 'Sam Wise', 1115556666, 'sw@Ayy.com',
-    	9456, NULL);" +
-    		   "INSERT INTO Agent_Represents VALUES (1567, 'Jack Li', 1118889999, 'jl@MAX.com',
-    	9567, 2);");
+    conn.query("INSERT INTO Agent_Represents VALUES (1123, 'Alex Huang', 1113456789, 'ah@real.com',9123, 10);" +
+    		   "INSERT INTO Agent_Represents VALUES (1234, 'Katherine Lab', 1113334444, 'kl@wb.com',9234, 9);" +
+    		   "INSERT INTO Agent_Represents VALUES (1345, 'Joe Baker', 1112223333, 'jb@rNOW.com',9345, 5);" +
+    		   "INSERT INTO Agent_Represents VALUES (1456, 'Sam Wise', 1115556666, 'sw@Ayy.com',9456, NULL);" +
+    		   "INSERT INTO Agent_Represents VALUES (1567, 'Jack Li', 1118889999, 'jl@MAX.com',9567, 2);");
 
-    conn.query("INSERT INTO Offer VALUES (2123, 'I love this house!', 
-    	TO_DATE('10-NOV-2014','DD-MM-YYYY'));" +
-  			  "INSERT INTO Offer VALUES (2234, 'When can I move in Kappa', 
-    	TO_DATE('05-MAR-2014','DD-MM-YYYY'));" +
-  			  "INSERT INTO Offer VALUES (2345, 'This house has same written all over it', 
-    	TO_DATE('30-OCT-2014','DD-MM-YYYY'));" +
-   			 "INSERT INTO Offer VALUES (3123, NULL, 
-    	TO_DATE('10-NOV-2014','DD-MM-YYYY'));" +
-	    	 "INSERT INTO Offer VALUES (3234, 'It's ok', 
-    	TO_DATE('01-JAN-2014','DD-MM-YYYY'));");
+    conn.query("INSERT INTO Offer VALUES (2123, 'I love this house!', TO_DATE('10-NOV-2014','DD-MM-YYYY'));" +
+  			  "INSERT INTO Offer VALUES (2234, 'When can I move in Kappa', TO_DATE('05-MAR-2014','DD-MM-YYYY'));" +
+  			  "INSERT INTO Offer VALUES (2345, 'This house has same written all over it', TO_DATE('30-OCT-2014','DD-MM-YYYY'));" +
+   			 "INSERT INTO Offer VALUES (3123, NULL, TO_DATE('10-NOV-2014','DD-MM-YYYY'));" +
+	    	 "INSERT INTO Offer VALUES (3234, 'It's ok', TO_DATE('01-JAN-2014','DD-MM-YYYY'));");
 
 
     // ALL BUYERS HAVE BUY IN THEIR EMAIL AS WELL AS A PHONE NUMBER STARTING WITH 604
@@ -62,24 +47,15 @@ exports.populate_tables = function() {
     		   "INSERT INTO Accepts VALUES (3123, '7784445555', 'Liam Neeson');" +
     		   "INSERT INTO Accepts VALUES (3234, '7785556666', 'Bob Jones');");
 
-    conn.query("INSERT INTO PurchaseOffer_Makes VALUES (2123, 500000, 8123, '6041112222', 
-    			'Pinkman Jones');" +
-    		   "INSERT INTO PurchaseOffer_Makes VALUES (2234, 1000000, 8234, '6042223333', 
-    		   	'Markus Lemonis');" +
-    		   "INSERT INTO PurchaseOffer_Makes VALUES (2345, 2000000, 8345, '6043334444', 
-    		   	'Angel Qin');");
+    conn.query("INSERT INTO PurchaseOffer_Makes VALUES (2123, 500000, 8123, '6041112222', 'Pinkman Jones');" +
+    		   "INSERT INTO PurchaseOffer_Makes VALUES (2234, 1000000, 8234, '6042223333', 'Markus Lemonis');" +
+    		   "INSERT INTO PurchaseOffer_Makes VALUES (2345, 2000000, 8345, '6043334444', 'Angel Qin');");
 
-    conn.query("INSERT INTO RentalOffer_Makes VALUES (3123, 1000, 8456, '604444555', 
-    			'Brando Wison');" +
-    			"INSERT INTO RentalOffer_Makes VALUES (3234, 1500, 8567, '6045556666', 
-    			'Hartof Lion');");
+    conn.query("INSERT INTO RentalOffer_Makes VALUES (3123, 1000, 8456, '604444555', 'Brando Wison');" +
+    			"INSERT INTO RentalOffer_Makes VALUES (3234, 1500, 8567, '6045556666', 'Hartof Lion');");
 
-    conn.query("INSERT INTO Appointment_View VALUES (123, 
-    			to_timestamp('2015/06/15 12:00', 'YYYY/MM/DD HH24 MI'), 1, 8123, '6041112222', 
-    			'Pinkman Jones');" +
-    			"INSERT INTO Appointment_View VALUES (234, 
-    			to_timestamp('2015/05/14 13:00', 'YYYY/MM/DD HH24 MI'), 2, 8234, '6042223333', 
-    			'Markus Lemonis');");
+    conn.query("INSERT INTO Appointment_View VALUES (123, to_timestamp('2015/06/15 12:00', 'YYYY/MM/DD HH24 MI'), 1, 8123, '6041112222', 'Pinkman Jones');" +
+    			"INSERT INTO Appointment_View VALUES (234, to_timestamp('2015/05/14 13:00', 'YYYY/MM/DD HH24 MI'), 2, 8234, '6042223333', 'Markus Lemonis');");
 
     conn.query("INSERT INTO Approves VALUES (123, 1123);" +
     	       "INSERT INTO Approves VALUES (234, 1345);");
@@ -111,10 +87,8 @@ exports.populate_tables = function() {
     		   "INSERT INTO PostSale VALUES (8456, 'Liam Neeson', '7784445555', 1456);" +
     		   "INSERT INTO PostSale VALUES (8567, 'Bob Jones', '7785556666', 1567);");
 
-    conn.query("INSERT INTO InterestedIn VALUES (8123, 'Pinkman Jones', '6041112222',
-    			'What a beautiful property, I want to buy this REAL QUICK');" +
-               "INSERT INTO InterestedIn VALUES (8234, 'Markus Lemonis', '6042223333',
-    			'This house will soon be MINE!!!');");
+    conn.query("INSERT INTO InterestedIn VALUES (8123, 'Pinkman Jones', '6041112222', 'What a beautiful property, I want to buy this REAL QUICK');" +
+               "INSERT INTO InterestedIn VALUES (8234, 'Markus Lemonis', '6042223333','This house will soon be MINE!!!');");
 
 
   });
