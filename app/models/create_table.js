18f87cacce6ee397386a0d5ed6ee7912b3e73191
qing yi li <qingyili@qing-yi-lis-iMac.local>
2015-06-13 21:42:55 -0700

    fixed add table

--- app/models/create_table.js
+++ app/models/create_table.js
@@ -3,52 +3,29 @@ var mysqlModule = require("../../mysqlModule");
 exports.initialize_tables = function() {
   mysqlModule.getConnection(function(err, conn) {
 
-    // clear all existing tuples 
-    conn.query("delete from Property_HasA_Location;");
-    conn.query("delete from Agency;");
-    conn.query("delete from Agent_Represents;");
-    conn.query("delete from Offer;");
-    conn.query("delete from Buyer;");
-    conn.query("delete from Seller;");
-    conn.query("delete from Accepts;");
-    conn.query("delete from PurchaseOffer_Makes;");
-    conn.query("delete from RentalOffer_Makes;");
-    conn.query("delete from Appointment_View;");
-    conn.query("delete from Approves;");
-    conn.query("delete from Rates;");
-    conn.query("delete from ForRent;");
-    conn.query("delete from ForSale;");
-    conn.query("delete from CommercialProperty_ForRent;");
-    conn.query("delete from CommercialProperty_ForSale;");
-    conn.query("delete from ResidentialProperty_ForRent;");
-    conn.query("delete from ResidentialProperty_ForSale;");
-    conn.query("delete from PostSale;");
-    conn.query("delete from InterestedIn;");
-
-    // drop all tables
-    conn.query("drop table Property_HasA_Location cascade constraints;" +
-                 "drop table Agency cascade constraints;" +
-                 "drop table Agent_Represents cascade constraints;" +
-                 "drop table Offer cascade constraints;" +
-                 "drop table Buyer cascade constraints;" +
-                 "drop table Seller cascade constraints;" +
-                 "drop table Accepts cascade constraints;" +
-                 "drop table PurchaseOffer_Makes cascade constraints;" +
-                 "drop table RentalOffer_Makes cascade constraints;" +
-                 "drop table Appointment_View cascade constraints;" +
-                 "drop table Approves cascade constraints;" +
-                 "drop table Rates cascade constraints;" +
-                 "drop table ForRent cascade constraints;" +
-                 "drop table ForSale cascade constraints;" +
-                 "drop table CommercialProperty_ForRent cascade constraints;" +
-                 "drop table CommercialProperty_ForSale cascade constraints;" +
-                 "drop table ResidentialProperty_ForRent cascade constraints;" +
-                 "drop table ResidentialProperty_ForSale cascade constraints;" +
-                 "drop table PostSale cascade constraints;" +
-                 "drop table InterestedIn cascade constraints;");
+    conn.query("drop table Property_HasA_Location;" +
+                 "drop table Agency;" +
+                 "drop table Agent_Represents;" +
+                 "drop table Offer;" +
+                 "drop table Buyer;" +
+                 "drop table Seller;" +
+                 "drop table Accepts;" +
+                 "drop table PurchaseOffer_Makes;" +
+                 "drop table RentalOffer_Makes;" +
+                 "drop table Appointment_View;" +
+                 "drop table Approves;" +
+                 "drop table Rates;" +
+                 "drop table ForRent;" +
+                 "drop table ForSale;" +
+                 "drop table CommercialProperty_ForRent;" +
+                 "drop table CommercialProperty_ForSale;" +
+                 "drop table ResidentialProperty_ForRent;" +
+                 "drop table ResidentialProperty_ForSale;" +
+                 "drop table PostSale;" +
+                 "drop table InterestedIn;");
 
     conn.query("CREATE TABLE Property_HasA_Location (" +
-                 "propertyID int PRIMARY KEY," +
+                 "propertyID smallint PRIMARY KEY," +
                  "isFurnished char(1)," +
                  "age smallint," +
                  "locationID smallint," +
@@ -77,7 +54,7 @@ exports.initialize_tables = function() {
     conn.query("CREATE TABLE Offer (" +
                  "offerID smallint PRIMARY KEY," +
                  "offerMessage varchar(200)," +
-                 "offerDate date)");
+                 "offerDate date not NULL)");
 
     conn.query("CREATE TABLE Buyer (" +
                  "buyerEmail varchar(15)," +
@@ -107,8 +84,7 @@ exports.initialize_tables = function() {
                  "buyerPhone char(10)," +
                  "buyerName varchar(20)," +
                  "FOREIGN KEY (propertyID) references Property_HasA_Location(propertyID)," +
-                 "FOREIGN KEY (buyerPhone, buyerName) references Buyer(buyerPhone, buyerName)," +
-                 "FOREIGN KEY (offerID) references Offer(offerID))");
+                 "FOREIGN KEY (buyerPhone, buyerName) references Buyer(buyerPhone, buyerName))");
 
     conn.query("CREATE TABLE RentalOffer_Makes (" +
                  "offerID smallint PRIMARY KEY," +
@@ -122,8 +98,8 @@ exports.initialize_tables = function() {
 
     conn.query("CREATE TABLE Appointment_View (" +
                  "appointmentID smallint PRIMARY KEY," + 
-                 "appointmentDate date," +
-                 "appointmentDuration integer not null," +
+                 "appointmentTime timestamp," +
+                 "appDuration integer not null," +
                  "propertyID smallint," + 
                  "buyerPhone char(10)," + 
                  "buyerName varchar(20)," + 
@@ -152,7 +128,7 @@ exports.initialize_tables = function() {
                  "FOREIGN KEY (propertyID) references Property_HasA_Location(propertyID))");
 
     conn.query("CREATE TABLE ForSale (" +
-                 "salePrice int not null," + 
+                 "salePrice smallint not null," + 
                  "propertyID smallint not null PRIMARY KEY," +
                  "FOREIGN KEY (propertyID) references Property_HasA_Location(propertyID))");
 
@@ -196,6 +172,10 @@ exports.initialize_tables = function() {
                  "message varchar(200)," +
                  "FOREIGN KEY (buyerPhone, buyerName) references Buyer(buyerPhone, buyerName)," +
                  "FOREIGN KEY (propertyID) references Property_HasA_Location(propertyID))");
+
+
+
+    mysqlModule.query(conn, "SELECT * FROM Agency;");
 
 
   });
