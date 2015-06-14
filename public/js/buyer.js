e010add34e1bf4c1c5eea0555c84ae26f380eaa4
qing yi li <qingyili@qing-yi-lis-iMac.local>
2015-06-13 17:42:48 -0700

    buyer server stuff added

---
+++ public/js/buyer.js
@@ -0,0 +1,9 @@
+$('#search-field').submit(function(ev) {
+  ev.preventDefault();
+  console.log("1");
+  var parameters = {search: $('#search').val() };
+  console.log(parameters);
+  $.get('/searching', parameters, function(data) {
+    console.log(data);
+  });
+});
