59208101710ca1555817bc70fce1958c29d2d3e1
Qing Yi Li <qinggyili@gmail.com>
2015-06-15 16:59:56 -0700

    fixed minor buyer mistakes

--- public/js/buyer.js
+++ public/js/buyer.js
@@ -10,14 +10,14 @@ $(window).load(function() {
   	$("#purchaseoffer > tbody").html("");
   	for (var i in data) {
   	  var rowString = "<tr>";
-      rowString += "<td>" + data[i].PropertyID + "</td>";
+      rowString += "<td>" + data[i].propertyID + "</td>";
       rowString += "<td>" + data[i].houseNumber + "</td>";
       rowString += "<td>" + data[i].street + "</td>";
       rowString += "<td>" + data[i].city + "</td>";
       rowString += "<td>" + data[i].age + "</td>";
       rowString += "<td>" + data[i].area + "</td>";
       rowString += "<td>" + data[i].isFurnished + "</td>";
-      rowString += "<td>" + data[i].OfferDate+ "</td>";
+      rowString += "<td>" + data[i].offerDate+ "</td>";
       rowString += "<td>" + data[i].purchaseAmount + "</td>";
       $("#purchaseoffer > tbody").append(rowString);
     }
