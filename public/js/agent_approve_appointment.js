96f6dd28bb13392030deb2f4e95d16383eb9f103
Jack <jackubc2013@gmail.com>
2015-06-16 17:49:37 -0700

    Check that the inputted value is valid

--- public/js/agent_approve_appointment.js
+++ public/js/agent_approve_appointment.js
@@ -6,13 +6,19 @@ $('#approve-field').submit(function(ev) {
   ev.preventDefault();
   var parameters = {appID: $('#approve').val() };
 
-  $.get('/agent_not_approved_appointmentID', parameters, function(data) {
-    for (var i in data) {
- 		if (parameters.appID == data[i].appointmentID) {
- 			$.get('/agent_appointment_approve', parameters, function(data) {
-
- 			})
+  if (parameters.appID > 32767 || parameters.appID < -32768) {
+  	Materialize.toast('Please enter a valid appointmentID', 4000);
+  } else {
+  	$.get('/agent_not_approved_appointmentID', parameters, function(data) {
+    	for (var i in data) {
+ 			if (parameters.appID == data[i].appointmentID) {
+ 				$.get('/agent_appointment_approve', parameters, function(data) {
+ 				})
+ 				Materialize.toast('Successfully approved, please refresh page', 4000);
+    		} else {
+    			Materialize.toast('Invalid appointmentID', 4000);
+    		}
     	}
-    }
-  })
+    })
+  }
 });
