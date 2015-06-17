2c4b74959242e569c957284c0d5db099ceeb9193
Alex Huang <h.alex38@gmail.com>
2015-06-17 00:00:30 -0700

    i duno

--- public/js/propertyfor.js
+++ public/js/propertyfor.js
@@ -1,26 +1,26 @@
- $('.datepicker').pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15 // Creates a dropdown of 15 years to control year
-  });
-      
+$(document).ready(function() {
+    $('select').material_select();
+});
 
-$('#purchase-offer-submit').click(function(ev) {
-  $('#purchase-offer-submit').hide();
+$('#property-for-sale-submit').click(function(ev) {
+  $('#property-for-sale-submit').hide();
   ev.preventDefault();
-  var oID = Math.floor(Math.random() * 327637);
-        $.get('/get_all_offerID', {}, function(data) {
-          while (data.indexOf(offerID) > -1)
-            oID = Math.floor(Math.random() * 32767);
-        });
 
-        parameters = { buyername: $('#buyername').val(),
-                      buyerphone:  $('#buyerphone').val(),
-                      propertyID:  $('#propertyID').val(),
-                      amount:  $('#amount').val(),
-                      message:  $('#message').val(),
-                      offerdate:  $('#offerdate').val(),
-                      offerID: oID};
+        parameters = { propertyID: $('#propertyID').val(),
+                      isfurnished:  $('#isfurnished').val(),
+                      age:  $('#age').val(),
+                      floors:  $('#floors').val(),
+                      bathrooms:  $('#bathrooms').val(),
+                      area:  $('#area').val(),
+                      locationID:  $('#locationID').val(),
+                      aptnumber:  $('#aptnumber').val(),
+                      housenumber:  $('#housenumber').val(),
+                      street:  $('#street').val(),
+                      country:  $('#country').val(),
+                      city:  $('#city').val(),
+                      province:  $('#province').val(),
+                      
         console.log(parameters);  
-          $.get('/create_new_purchase_offer', parameters, function(data){});
+          $.get('/create_new_property_for_sale', parameters, function(data){});
  $('#form').hide();
   })
